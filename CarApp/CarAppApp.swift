e8a10f1deb879586f16cb00c69942877e6a80b92
Ronado Vargas <ronaldovh99@gmail.com>
2026-02-17 18:33:52 -0500

    Initial Commit

---
+++ CarApp/CarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAppApp.swift
+//  CarApp
+//
+//  Created by Ronaldo Andre on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
