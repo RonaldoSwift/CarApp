e2248a17e5662bf85bda7286de70195013576ffb
Ronado Vargas <ronaldovh99@gmail.com>
2026-02-18 08:39:39 -0500

    Add HomeView, Fonts, Image,Color.

---
+++ CarApp/Presentation/HomeView.swift
@@ -0,0 +1,56 @@
+//
+//  HomeView.swift
+//  CarApp
+//
+//  Created by Ronaldo Andre on 17/02/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color(Color.colorFondoHome)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                
+                Image("carFortuner")
+                    .resizable()
+                    .scaledToFit()
+                    .scaledToFill()
+                    .frame(height: 400)
+                    .opacity(0.9)
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    
+                    Text("Premium cars.\nEnjoy the luxury")
+                        .font(.custom("Barlow-Bold", size: 35.2))
+                        .foregroundColor(.white)
+                        .padding(.top, 10)
+                    
+                    Text("Premium and prestige car daily rental.\nExperience the thrill at a lower price")
+                        .font(.custom("Barlow-Regular", size: 15))
+                        .foregroundColor(.white)
+                        .padding(.top, 5)
+                    
+                    Button(action: {}) {
+                        Text("Let's Go")
+                            .font(.custom("Barlow-Bold", size: 20))
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .foregroundColor(.black)
+                            .clipShape(Capsule())
+                    }
+                    .padding(.top, 40)
+                }
+                .padding(.horizontal, 24)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
